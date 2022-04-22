5ff4356124071aea0e1fa8bd96e3aec0a76a3c68
SWAROOP R <60713891+FORSWAROOPRAJ@users.noreply.github.com>
2022-04-22 18:31:55 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://jwtpraticeguvi.herokuapp.com/';
 
 export default axios.create({
     baseURL: BASE_URL
